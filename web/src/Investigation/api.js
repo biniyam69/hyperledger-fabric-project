e06de8424853450d5ca9df2559e7c10a89e58da0
Biniyam Ajaw <91191700+biniyam69@users.noreply.github.com>
2023-06-27 02:36:36 +0000

    Added routes for court

--- web/src/Investigation/api.js
+++ web/src/Investigation/api.js
@@ -2,25 +2,86 @@
 
 import fetch from 'isomorphic-fetch';
 
-export function getRepairOrders() {
-  return fetch('/repair-shop/api/repair-orders', {
-    method: 'POST',
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    })
-  }).then(async res => {
-    return await res.json();
-  });
-}
-
-export function completeRepairOrder(uuid) {
-  return fetch('/repair-shop/api/complete-repair-order', {
+export function createInvestigation(id, caseNumber, description) {
+  return fetch('/investigation/api/create-investigation', {
     method: 'POST',
     headers: new Headers({
       'Content-Type': 'application/json'
     }),
-    body: JSON.stringify({ uuid })
+    body: JSON.stringify({ id, caseNumber, description })
   }).then(async res => {
-    return await res.json();
+    const response = await res.json();
+    if (response.success) {
+      return response.message;
+    } else {
+      throw new Error(response.error);
+    }
+  });
+}
+
+export function getInvestigation(id) {
+  return fetch(`/investigation/api/get-investigation/${id}`, {
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  }).then(async res => {
+    const response = await res.json();
+    if (response.success) {
+      return response.investigation;
+    } else {
+      throw new Error(response.error);
+    }
+  });
+}
+
+export function addEvidence(id, evidence) {
+  return fetch(`/investigation/api/add-evidence/${id}`, {
+    method: 'PUT',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify({ evidence })
+  }).then(async res => {
+    const response = await res.json();
+    if (response.success) {
+      return response.message;
+    } else {
+      throw new Error(response.error);
+    }
+  });
+}
+
+export function assignInvestigator(id, investigator) {
+  return fetch(`/investigation/api/assign-investigator/${id}`, {
+    method: 'PUT',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify({ investigator })
+  }).then(async res => {
+    const response = await res.json();
+    if (response.success) {
+      return response.message;
+    } else {
+      throw new Error(response.error);
+    }
+  });
+}
+
+export function changeInvestigationStatus(id, status) {
+  return fetch(`/investigation/api/change-investigation-status/${id}`, {
+    method: 'PUT',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify({ status })
+  }).then(async res => {
+    const response = await res.json();
+    if (response.success) {
+      return response.message;
+    } else {
+      throw new Error(response.error);
+    }
   });
 }
--- web/www/blockchain/investigationPeer.js
+++ web/www/blockchain/investigationPeer.js
@@ -2,93 +2,65 @@
 
 import config from './config';
 import { wrapError, marshalArgs } from './utils';
-import { shopClient as client, isReady } from './setup';
-import uuidV4 from 'uuid/v4';
+import { investigationClient as client, isReady } from './setup';
 
 import network from './invoke';
 
-import * as util from 'util' // has no default export
+import * as util from 'util'; // has no default export
 
-export async function getContractTypes(shopType) {
+export async function createInvestigation(id, caseNumber, description) {
   if (!isReady()) {
     return;
   }
   try {
-    return await query('contract_type_ls', { shopType });
+    await invoke('createInvestigation', id, caseNumber, description);
   } catch (e) {
-    throw wrapError(
-      `Error getting contract types for shop type ${shopType} : ${e.message}`
-      , e);
+    throw wrapError(`Error creating investigation ${e.message}`, e);
   }
 }
 
-export async function createContract(contract) {
+export async function getInvestigation(id) {
   if (!isReady()) {
     return;
   }
   try {
-    console.log(`contract before ojbect.assign: ${contract}`)
-    let c = Object.assign({}, contract, { uuid: uuidV4() });
-    console.log(`contract after object.assign: ${c}`)
-    const loginInfo = await invoke('contract_create', c);
-    if (!loginInfo
-      ^ !!(loginInfo && loginInfo.username && loginInfo.password)) {
-      return Object.assign(loginInfo || {}, { uuid: c.uuid });
-    } else {
-      throw new Error(loginInfo);
-    }
+    return await query('getInvestigation', id);
   } catch (e) {
-    throw wrapError(`Error creating contract: ${e.message}`, e);
+    throw wrapError(`Error getting investigation ${e.message}`, e);
   }
 }
 
-export async function createUser(user) {
+export async function addEvidence(id, evidence) {
   if (!isReady()) {
     return;
   }
   try {
-    const loginInfo = await invoke('user_create', user);
-    if (!loginInfo ^
-      !!(loginInfo && loginInfo.username && loginInfo.password)) {
-      return loginInfo;
-    } else {
-      throw new Error(loginInfo);
-    }
+    await invoke('addEvidence', id, evidence);
   } catch (e) {
-    throw wrapError(`Error creating user: ${e.message}`, e);
+    throw wrapError(`Error adding evidence to investigation ${e.message}`, e);
   }
 }
 
-export async function authenticateUser(username, password) {
+export async function assignInvestigator(id, investigator) {
   if (!isReady()) {
     return;
   }
   try {
-    let authenticated =
-      await query('user_authenticate', { username, password });
-    if (authenticated === undefined || authenticated === null) {
-      throw new Error('Unknown error, invalid response!');
-    }
-    return authenticated;
+    await invoke('assignInvestigator', id, investigator);
   } catch (e) {
-    throw wrapError(`Error authenticating user: ${e.message}`, e);
+    throw wrapError(`Error assigning investigator to investigation ${e.message}`, e);
   }
 }
 
-export async function getUserInfo(username) {
+export async function changeInvestigationStatus(id, status) {
   if (!isReady()) {
     return;
   }
   try {
-    const user = await query('user_get_info', { username });
-    return user;
+    await invoke('changeInvestigationStatus', id, status);
   } catch (e) {
-    throw wrapError(`Error getting user info: ${e.message}`, e);
+    throw wrapError(`Error changing investigation status ${e.message}`, e);
   }
-}
-
-export function getBlocks(noOfLastBlocks) {
-  return client.getBlocks(noOfLastBlocks);
 }
 
 export const on = client.on.bind(client);
@@ -97,40 +69,32 @@ export const addListener = client.addListener.bind(client);
 export const prependListener = client.prependListener.bind(client);
 export const removeListener = client.removeListener.bind(client);
 
-//identity to use for submitting transactions to smart contract
-const peerType = 'shopApp-admin'
+// Identity to use for submitting transactions to the smart contract
+const peerType = 'investigationApp-admin';
 let isQuery = false;
 
 async function invoke(fcn, ...args) {
-
   isQuery = false;
-  
-  console.log(`args in shopPeer invoke: ${util.inspect(...args)}`)
-  console.log(`func in shopPeer invoke: ${util.inspect(fcn)}`)
+
+  console.log(`args in investigationPeer invoke: ${util.inspect(...args)}`);
+  console.log(`func in investigationPeer invoke: ${util.inspect(fcn)}`);
 
   if (config.isCloud) {
     await network.invokeCC(isQuery, peerType, fcn, ...args);
   }
 
-  return client.invoke(
-    config.chaincodeId, config.chaincodeVersion, fcn, ...args);
-
+  return client.invoke(config.chaincodeId, config.chaincodeVersion, fcn, ...args);
 }
 
 async function query(fcn, ...args) {
-
   isQuery = true;
 
-  console.log(`args in shopPeer query: ${util.inspect(...args)}`)
-  console.log(`func in shopPeer query: ${util.inspect(fcn)}`)
-
-  console.log('config.isCLoud: ');
-  console.log(config.isCloud);
+  console.log(`args in investigationPeer query: ${util.inspect(...args)}`);
+  console.log(`func in investigationPeer query: ${util.inspect(fcn)}`);
 
   if (config.isCloud) {
     await network.invokeCC(isQuery, peerType, fcn, ...args);
   }
 
-  return client.query(
-    config.chaincodeId, config.chaincodeVersion, fcn, ...args);
+  return client.query(config.chaincodeId, config.chaincodeVersion, fcn, ...args);
 }
